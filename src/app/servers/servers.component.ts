17377f4e7eb5ef12e471efe916044520022a8311
Egzon <egzon@starlabspro.com>
2021-06-01 22:40:38 +0200

    feat: using ngIf statement to change data conditionallity

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   allowNewServer=false;
   serverCreationStatus="No server was created!";
   serverName='TestServer';
+  serverCreated=false;
   constructor() {
     setTimeout(()=>{
       this.allowNewServer=true
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
   onCreateServer(){
+    this.serverCreated=true
     this.serverCreationStatus="Server Was created! Name is " + this.serverName
   }
   onUpdateServerName(event:any){
