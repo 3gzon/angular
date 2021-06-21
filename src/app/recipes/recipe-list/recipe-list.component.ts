ee0c300ae37d6ee5b9832a3fd5fca111d9960673
Egzon Uka <egzonuka@q-pc.ikub>
2021-06-21 14:48:43 +0200

    feat: managing recipes in recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) { }
 
-    new Recipe('A test recipe name one', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752'),
-    new Recipe('A test recipe name two', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752')
-  ];
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
   }
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe)
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,13 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+
+        new Recipe('A test recipe name one', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752'),
+        new Recipe('A test recipe name two', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
