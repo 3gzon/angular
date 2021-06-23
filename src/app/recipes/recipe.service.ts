a33f7dadf1bf519f0f56a2a0b8edc16e22bf5fcb
Egzon Uka <egzonuka@q-pc.ikub>
2021-06-23 14:41:11 +0200

    passing ingredients from recipes to the shoppng list via services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,8 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredeint } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
-
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
@@ -14,13 +15,16 @@ export class RecipeService {
         ),
         new Recipe('A test recipe name two', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752',
             [
-                new Ingredeint('lattuce', 10),
+                new Ingredeint('lettuce', 10),
                 new Ingredeint('orange peace', 3)
             ]
         )
     ];
-
+    constructor(private slService: ShoppingListService) { }
     getRecipes() {
         return this.recipes.slice();
+    }
+    addIngredientsToShoppingList(ingredients: Ingredeint[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
