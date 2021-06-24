aa7b4bf7b1ee81a19a9af85fb96ef6181d98147b
Egzon Uka <egzonuka@q-pc.ikub>
2021-06-24 09:30:44 +0200

    configuring routing parametes, get recipes with dynamic id

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,11 +10,9 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeServices: RecipeService) { }
+
 
   ngOnInit(): void {
   }
-  onSelected() {
-    this.recipeServices.recipeSelected.emit(this.recipe)
-  }
+
 }
