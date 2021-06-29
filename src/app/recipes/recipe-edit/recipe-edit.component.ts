d3de152ff53baf1a47de861712e2a6610d64ec4c
Egzon Uka <egzonuka@q-pc.ikub>
2021-06-29 11:43:30 +0200

    creating the form for edditing recipes, I separate with if condition for editmoe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,7 +11,8 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false
-  constructor(private route: ActivatedRoute) { }
+  recipeForm: FormGroup
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params
@@ -17,9 +20,25 @@ export class RecipeEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
-          console.log(this.editMode);
+          this.initForm();
         }
       )
   }
+  private initForm() {
+    let recipeName = '';
+    let recipeImagepath = '';
+    let recipeDescription = '';
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagepath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
 
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagepath),
+      'description': new FormControl(recipeDescription)
+    })
+  }
 }
