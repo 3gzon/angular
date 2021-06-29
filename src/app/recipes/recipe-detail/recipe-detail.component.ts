f3567b0057ca04be64aac26dbeeeeea95b05ebf9
Egzon Uka <egzonuka@q-pc.ikub>
2021-06-29 12:04:37 +0200

    syncing html and logic forms

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -22,6 +22,7 @@ export class RecipeDetailComponent implements OnInit {
         }
       )
   }
+
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
   }
