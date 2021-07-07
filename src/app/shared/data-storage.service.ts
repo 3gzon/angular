516845a24f58c441edfe0cc2ef670238ff770b22
Egzon Uka <egzonuka@Egzons-MacBook-Pro.local>
2021-07-07 16:24:55 +0200

    storing the data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient) {
-
+    constructor(private http: HttpClient, private recipesService: RecipeService) { }
+    storeRecipes() {
+        const recipes = this.recipesService.getRecipes();
+        return this.http.put('https://recipe-76112-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+            console.log(response);
+        });
     }
+
 }
