641f7273b9db5c54a998c9ba17dea9c5cc18508e
Egzon <egzonuka8@gmail.com>
2021-09-30 16:25:59 +0200

    transformin data with pipe and map observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,21 +6,22 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>()
-    private recipes: Recipe[] = [
+    // private recipes: Recipe[] = [
 
-        new Recipe('A test recipe name one', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752',
-            [
-                new Ingredeint('juice', 2),
-                new Ingredeint('orange juice', 3)
-            ]
-        ),
-        new Recipe('A test recipe name two', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752',
-            [
-                new Ingredeint('lettuce', 10),
-                new Ingredeint('orange peace', 3)
-            ]
-        )
-    ];
+    //     new Recipe('A test recipe name one', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752',
+    //         [
+    //             new Ingredeint('juice', 2),
+    //             new Ingredeint('orange juice', 3)
+    //         ]
+    //     ),
+    //     new Recipe('A test recipe name two', 'this is a test description', 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752',
+    //         [
+    //             new Ingredeint('lettuce', 10),
+    //             new Ingredeint('orange peace', 3)
+    //         ]
+    //     )
+    // ];
+    private recipes: Recipe[] = [];
     constructor(private slService: ShoppingListService) { }
     setRecipes(recipes: Recipe[]) {
         this.recipes = this.recipes
