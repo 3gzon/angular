ae8828f3f93b4ba82861de4b8ed92b0070d47a2d
Egzon <egzonuka8@gmail.com>
2021-10-04 16:52:39 +0200

    created a form with formcontrol names

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,24 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
     selector: "app-auth",
     templateUrl: "./auth.component.html"
 })
 export class AuthComponent {
+    authForm: FormGroup;
     isLoginMode = true;
+    ngOnInit(): void {
+        this.buildForm();
+    }
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode
+    }
+    private buildForm(): void {
+        this.authForm = new FormGroup({
+            email: new FormControl(),
+            password: new FormControl()
+        })
+        console.log(this.authForm);
     }
 }
